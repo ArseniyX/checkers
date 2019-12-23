4efb421bb058809b6c5621eb4df9eceaaab28097
Arseniy <jkl@usa.com>
2019-12-23 09:35:46 +0200

    renaming

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function showThat() {
-    alert('Hello World!');
+    const age = Number(prompt("Write your age:"));
+    if (age <= 16) {
+        alert("You can't ride");
+    } else {
+        alert("You can ride")
+    }
 }
